825c7d8c1064bce6fa55485fc449ca21a4668fde
Xavi <xavierkas09@gmail.com>
2025-07-25 11:12:57 +0800

    - Added login to the main route

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,7 @@ import ShippingPolicyPage from './pages/ShippingPolicy';
 import {Checkout} from './pages/Checkout';
 import {Account} from './pages/Account';
 import Signup from './pages/Signup';
+import Login from './pages/Login';
 
 export function App() {
   return (
@@ -55,6 +56,7 @@ export function App() {
                 <Route path="/checkout"  element={<Checkout />} />
                 <Route path="/account"  element={<Account />} />
                 <Route path="/signup"  element={<Signup />} />
+                <Route path="/login" element={<Login />} />
               </Routes>
             </main>
             <Footer />
