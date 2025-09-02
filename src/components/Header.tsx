b238cdfad5dd38ea002b6dfadfc01ac6d3e94285
Xavi <xavierkas09@gmail.com>
2025-09-02 09:50:04 +0800

    feat(Header):Search Bar - Changed the color of the focus around the search box

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -63,7 +63,7 @@ const Header = () => {
               <input
                 ref={searchInputRef}
                 type="text"
-                className="w-64 px-3 py-2 border border-green-500 rounded-md shadow focus:outline-none focus:ring-2 focus:ring-green-500 transition"
+                className="w-64 px-3 py-2 border border-green-500 rounded-md shadow focus:outline-none focus:ring-2 focus:ring-green-700 transition"
                 placeholder="Search..."
                 value={searchValue}
                 onChange={e => setSearchValue(e.target.value)}
@@ -76,7 +76,7 @@ const Header = () => {
                 }}
               />
             ) : (
-              <button className="p-1 rounded-full text-gray-500 hover:text-green-700" onClick={() => setShowSearch(true)}>
+              <button className="p-1 rounded-full text-gray-500 hover:text-black-700" onClick={() => setShowSearch(true)}>
                 <SearchIcon className="h-6 w-6" />
               </button>
             )}
