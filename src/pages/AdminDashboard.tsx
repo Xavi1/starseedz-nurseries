eabfc47e764b6ad7f0c4c034e22bc4d3f579f7af
Xavi <xavierkas09@gmail.com>
2026-02-10 14:03:55 +0800

    feat: Added Product List import to be factored in Later.

--- src/pages/AdminDashboard.tsx
+++ src/pages/AdminDashboard.tsx
@@ -21,6 +21,7 @@ import DashboardMainLayout from './AdminDashboard/layout/DashboardMainLayout';
 import  OrderDetail  from "./AdminDashboard/Orders/OrderDetail";
 import ProductDetail from "./AdminDashboard/Products/ProductDetail";
 import ProductEditModal from './AdminDashboard/Products/ProductEditModal';
+import ProductsList from './AdminDashboard/Products/ProductsList';
 
 // =============================
 // AdminDashboard.tsx
